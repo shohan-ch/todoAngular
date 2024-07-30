bd5b064b31f56f4ddf83509e268db894be016d46
shohan-ch <shohan889@yahoo.com>
2024-07-30 21:58:01 +0600

    feat: route added for dashboard & todos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TodoComponent } from './components/todo/todo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'todos', component: TodoComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+];
