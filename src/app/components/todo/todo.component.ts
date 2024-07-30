845c13b5467d28b45dd8f9e47d138ac9e8ea5fc7
shohan-ch <shohan889@yahoo.com>
2024-07-30 23:00:42 +0600

    todo service & table data shows

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule, JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { NzButtonComponent, NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzTableModule } from 'ng-zorro-antd/table';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [],
+  imports: [
+    NzTableModule,
+    NzDividerModule,
+    JsonPipe,
+    CommonModule,
+    NzButtonComponent,
+    NzButtonModule,
+  ],
   templateUrl: './todo.component.html',
-  styleUrl: './todo.component.css'
+  styleUrl: './todo.component.css',
 })
 export class TodoComponent {
-
+  todos = [
+    {
+      name: 'Eat',
+      description: 'Need to eat for living',
+      status: 'active',
+    },
+    {
+      name: 'Sleep',
+      description: 'Sleep is mandatory',
+      status: 'pending',
+    },
+  ];
 }
