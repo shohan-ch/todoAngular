62a147589ad40e12d12e17457a82227857ecb85e
shohan-ch <shohan889@yahoo.com>
2024-08-01 00:37:46 +0600

    login component added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  NgForm,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  isSubmit = false;
+  formData = {
+    userName: '',
+    password: '',
+  };
 
+  handleSubmit(form: NgForm) {
+    this.isSubmit = true;
+    if (form.valid) {
+      console.log(this.formData);
+      form.reset();
+      this.isSubmit = false;
+    }
+  }
 }
