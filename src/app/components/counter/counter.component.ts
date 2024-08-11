380d1cbd741fd597de958596a0950f08a12b876a
ch <shohan889@yahoo.comm>
2024-08-11 21:28:51 +0600

    ngrx state managment added

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { of } from 'rxjs';
 @Component({
   selector: 'app-counter',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './counter.component.html',
-  styleUrl: './counter.component.css'
+  styleUrl: './counter.component.css',
 })
 export class CounterComponent {
-
+  // counter$;
+  counter$ = of(10);
+  // counter$ = 10;
 }
